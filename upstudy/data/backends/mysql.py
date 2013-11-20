3de9e2507304133bc5c81aa0ec66d16ddda92d43
Olivier Yiptong <olivier@olivieryiptong.com>
2013-11-20 16:47:44 -0500

    mysql backend schema revised to capture payload data

--- upstudy/data/backends/mysql.py
+++ upstudy/data/backends/mysql.py
@@ -31,20 +31,35 @@ class MySQLBackend(SQLBackend):
                     "FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE" +
                 ") ENGINE=InnoDB;",
 
-                "CREATE TABLE submission_data (" +
-                    "user_id INTEGER UNSIGNED NOT NULL, " +
-                    "type_id INTEGER UNSIGNED NOT NULL, " +
-                    "namespace_id INTEGER UNSIGNED NOT NULL, " +
-                    "category_id INTEGER UNSIGNED NOT NULL, " +
-                    "FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, " +
-                    "FOREIGN KEY (type_id) REFERENCES types(id) ON DELETE CASCADE, " +
-                    "FOREIGN KEY (namespace_id) REFERENCES namespaces(id) ON DELETE CASCADE, " +
-                    "FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE" +
+                "CREATE TABLE submissions (" +
+                    "id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "user_id INTEGER UNSIGNED NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, " +
+                    "timestamp_received TIMESTAMP NOT NULL, INDEX timestamp_received_idx USING BTREE (timestamp_received), " +
+                    "timestamp_installed TIMESTAMP NOT NULL, INDEX timestamp_installed_idx USING BTREE (timestamp_installed), " +
+                    "timestamp_payload TIMESTAMP NOT NULL, INDEX timestamp_payload_idx USING BTREE (timestamp_payload), " +
+                    "timestamp_upload TIMESTAMP NOT NULL, INDEX timestamp_upload_idx USING BTREE (timestamp_upload), " +
+                    "source VARCHAR(255) NOT NULL, INDEX source_idx USING BTREE (source), " +
+                    "locale VARCHAR(255) NOT NULL, INDEX locale_idx USING BTREE (locale), " +
+                    "addon_version VARCHAR(255) NOT NULL, INDEX addon_version_idx USING BTREE (addon_version), " +
+                    "tld_counter TEXT NOT NULL, " +
+                    "prefs TEXT NOT NULL" +
+                ") ENGINE=InnoDB;",
+
+                "CREATE TABLE submission_interests (" +
+                    "user_id INTEGER UNSIGNED NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE, " + # denormalize user_id
+                    "submission_id INTEGER UNSIGNED NOT NULL, FOREIGN KEY (submission_id) REFERENCES submissions(id) ON DELETE CASCADE, " +
+                    "type_id INTEGER UNSIGNED NOT NULL, FOREIGN KEY (type_id) REFERENCES types(id) ON DELETE CASCADE, " +
+                    "namespace_id INTEGER UNSIGNED NOT NULL, FOREIGN KEY (namespace_id) REFERENCES namespaces(id) ON DELETE CASCADE, " +
+                    "category_id INTEGER UNSIGNED NOT NULL, FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE, " +
+                    "day INTEGER UNSIGNED NOT NULL, INDEX day_idx USING BTREE (day), " +
+                    "hostCount TEXT NOT NULL, " +
+                    "PRIMARY KEY (user_id, type_id, namespace_id, category_id, day)" +
                 ") ENGINE=InnoDB;"
             ],
 
             "teardown": [
-                "DROP TABLE IF EXISTS submission_data CASCADE;",
+                "DROP TABLE IF EXISTS submission_interests CASCADE;",
+                "DROP TABLE IF EXISTS submissions CASCADE;",
                 "DROP TABLE IF EXISTS survey_data CASCADE;",
                 "DROP TABLE IF EXISTS categories CASCADE;",
                 "DROP TABLE IF EXISTS namespaces CASCADE;",
